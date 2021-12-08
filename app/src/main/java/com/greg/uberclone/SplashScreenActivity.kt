a023c3042b54aad1d1168e4e49bc9e52bf085e4a
GregBiglione <gregory.biglione81@gmail.com>
2021-12-08 23:30:27 +0100

    Added SplashScreenActivity

--- app/src/main/java/com/greg/uberclone/SplashScreenActivity.kt
+++ app/src/main/java/com/greg/uberclone/SplashScreenActivity.kt
@@ -3,14 +3,15 @@ package com.greg.uberclone
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.greg.uberclone.databinding.ActivityMainBinding
+import com.greg.uberclone.databinding.ActivitySplashScreenBinding
 
-class MainActivity : AppCompatActivity() {
+class SplashScreenActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivitySplashScreenBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
     }
