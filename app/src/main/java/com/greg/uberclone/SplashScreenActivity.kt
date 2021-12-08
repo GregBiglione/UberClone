bedb96155b212cdeeab0270252a1ea75f7b62ba9
GregBiglione <gregory.biglione81@gmail.com>
2021-12-09 00:40:00 +0100

    Added styles & splashTimer() in SplashScreenActivity

--- app/src/main/java/com/greg/uberclone/SplashScreenActivity.kt
+++ app/src/main/java/com/greg/uberclone/SplashScreenActivity.kt
@@ -2,8 +2,13 @@ package com.greg.uberclone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.droidman.ktoasty.KToasty
 import com.greg.uberclone.databinding.ActivityMainBinding
 import com.greg.uberclone.databinding.ActivitySplashScreenBinding
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.core.Completable
+import java.util.concurrent.TimeUnit
 
 class SplashScreenActivity : AppCompatActivity() {
 
@@ -14,5 +19,18 @@ class SplashScreenActivity : AppCompatActivity() {
         binding = ActivitySplashScreenBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+        splashTimer()
+    }
+
+    //----------------------------------------------------------------------------------------------
+    //----------------------- Timer ----------------------------------------------------------------
+    //----------------------------------------------------------------------------------------------
+
+    private fun splashTimer(){
+        Completable.timer(3, TimeUnit.SECONDS, AndroidSchedulers.mainThread())
+            .subscribe {
+                KToasty.success(this@SplashScreenActivity, "Splash screen run done !",
+                    Toast.LENGTH_SHORT).show()
+            }
     }
 }
